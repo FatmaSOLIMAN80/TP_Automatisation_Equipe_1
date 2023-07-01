5a84ca0c743d0f75c54108baeb08652360f43bd5
soumaya <soumaya1.haddad@gmail.com>
2023-07-01 18:49:49 -0400

    MAJ01-07

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   pageLoadTimeout: 20000,
   e2e: {
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    env: {
+      "LANGUAGE": "en"
+    },
     setupNodeEvents(on, config) {
     },
   },
--- cypress/e2e/loguin_logout_test.cy.ts
+++ cypress/e2e/loguin_logout_test.cy.ts
@@ -1,7 +1,16 @@
 describe('vérifier le loguin et logout', () => {
     beforeEach(() => {
-        cy.visit('https://opensource-demo.orangehrmlive.com', {
+        cy.clearAllCookies();
+        cy.clearAllSessionStorage();
+        /*/cy.visit('https://opensource-demo.orangehrmlive.com', {
             timeout: 50000
+        });*/
+        cy.visit('https://opensource-demo.orangehrmlive.com', {
+            onBeforeLoad(win) {
+                Object.defineProperty(win.navigator, 'language', {
+                    value: Cypress.env('LANGUAGE'),
+                });
+            },
         });
         cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear();
         cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
@@ -21,4 +30,23 @@ describe('vérifier le loguin et logout', () => {
         cy.get(':nth-child(4) > .oxd-userdropdown-link').click();
         cy.get('.oxd-text--h5').contains("Login")
     });
-});
+
+    /* it('ajout d\'un Admin ', () => {
+         cy.get(':nth-child(1) > .oxd-main-menu-item > .oxd-text').click()
+         cy.get('.orangehrm-header-container > .oxd-button').click()
+         cy.get('.oxd-select-text-input').first().click().then(() => {
+             cy.get('div[role="option"] > span')
+                 .contains('Admin')
+                 .click();
+         })
+ 
+     });*/
+    it('ajout d\'un Admin', () => {
+        cy.contains('.oxd-main-menu-item > .oxd-text', 'Admin').click();
+        cy.get('.orangehrm-header-container > .oxd-button').click();
+        cy.get('.oxd-select-text-input').first().click();
+        cy.contains('div[role="option"] > span', 'Admin').click();
+    });
+
+
+})
